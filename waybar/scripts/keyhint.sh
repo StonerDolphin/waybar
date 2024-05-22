5d24cb29d00f163a862208359e0560fbee19a233
Camilo Triviño <cam.trivino@gmail.com>
2024-05-22 10:06:02 -0400

    Add files via upload

--- waybar/scripts/keyhint.sh
+++ waybar/scripts/keyhint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-W=$(xrandr -q | grep -w Screen | sed 's/.*current //;s/,.*//' | awk '{print $1}')
-H=$(xrandr -q | grep -w Screen | sed 's/.*current //;s/,.*//' | awk '{print $3}')
-pointX=$(echo $((($W-500)/2)))
-pointY=$(echo $((($H-500)/2)))
-yad --width=850 --height=500 \
+# "Change keyboard layout in" "~/.config/hypr/hyprland.conf" " " \
+
+yad --width=530 --height=620 \
+--center \
 --fixed \
---geometry="+$pointX+$pointY" \
 --title="Garuda Hyprland Keybindings" \
 --no-buttons \
 --list \
@@ -15,19 +13,24 @@ yad --width=850 --height=500 \
 --timeout=60 \
 --timeout-indicator=right \
 "ESC" "close this app" "" "=" "modkey" "(set mod Mod4)" \
+"+Shift+g" "G-hyprland" "implement G-Hyprland" \
 "+enter/+t" "Terminal" "(foot)" \
 "+d" "Application Menu" "(wofi)" \
 "+Shift+d" "Full Launcher" "(nwggrid)" \
 "+o" "" "Open Broswer" \
 "+n" "" "Open Files" \
 "+q" "close focused app" "(kill)" \
-"Print-key" "screenshot" "(grim)" \
+"+v" "clipboard manager" "(cliphist)" \
+"+r" "Resize windows" "(resize)" \
+"Print" "screenshot" "(grimblast)" \
+"Shift+Print" "screenshot region" "(grimblast)" \
+"+Print" "screenshot window" "(grimblast)" \
 "+Shift+e" "power-menu" "(wofi)" \
 "+Shift+c" "Change wallpaper" "(wpaperd)" \
-"+f" "Fullscreen" "Toggles to fullscreen" \
-"+Shift+f" "Fake fullscreen" "Makes open window to behave as fullscreen without changing geoometry" \
+"+f" "Fullscreen" "Toggles to full screen" \
+"+Shift+f" "Fake fullscreen" "Behave full screen without full screen" \
 "+Shift+Spacebar" "Float" "Toggle windows to float" \
 "+p" "Dwindle effect" "pseudo" \
-"+j" "Dwindle effect" "toggle split" \
-"Change keyboard layout in" "~/.config/hypr/hyprland.conf" " " \
-"Window closed in 60 sec."\
+"+Shift+p" "Dwindle effect" "toggle split" \
+"+i" "Calamares" "Install Garuda Hyprland" \
+"" "" "     Window closed in 60 sec."\
